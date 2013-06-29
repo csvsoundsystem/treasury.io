81b7fe79c31ff08ee941f441dd06064cf3e8fdc2
Michael Keller <michael.keller@gmail.com>
2013-06-29 00:53:02 -0400

    No more heroku app for csv generation, download is done by transferring type/csv MIME type, reordered buttons to favor JSON, added dsv library which potentially disables IE, to check, cleaned up jquery vars

--- web/js/main.js
+++ web/js/main.js
@@ -1,7 +1,13 @@
 $(function() {
-  var encoding = false;
-  var $query_refresher = $('#query-refresher');
+  var encoding = false,
+      api_endpoint = 'https://premium.scraperwiki.com/cc7znvq/47d80ae900e04f2/sql/?q=',
+      $query_refresher = $('#query-refresher'),
+      $download_csv_btn = $('#download-csv'),
+      $download_json_btn = $('#download-json'),
+      $sql_query_textarea = $('#sql');
+
   function bindHandlers(){
+      /* NAV MENU BEHAVIOR */
       $('#navmenu').scrollSpy()
 
       $('#navmenu ul li a').on('click', function(e) {
@@ -14,29 +20,41 @@ $(function() {
           scrollThere(that, e);
       });
 
+      /* QUERY BOX BEHAVIOR */
+      /* QUERY ENCODING */
       $("#query-encoding-options input[name='encoding']").change(function(){
         var query_text = $("#sql").val();
         encoding = $(this).val();
         if(encoding == 'true'){
            var encoded_text = encodeURI(query_text);
-           $("#sql").val(encoded_text);
+           $sql_query_textarea.val(encoded_text);
          }else{
            var unencoded_text = decodeURI(query_text);
-           $("#sql").val(unencoded_text);
+           $sql_query_textarea.val(unencoded_text);
          }
       });
 
+      /* QUERY REFRESHING */
       $('#query').on('keydown', '.gwt-TextBox', function(){
           $query_refresher.removeAttr('disabled');
       });
 
+      /* DISABLE FIRST LIST ITEM, COULD BE IMPROVED  TO BE DISABLED FROM THE BEGINNING BUT THAT WAS CAUSING PROBLEMS WITH RQB */
       $('#query').on('change', '.gwt-ListBox', function(){
         if ($('#rqb .gwt-ListBox option:first-child').attr('disabled') == undefined){
           $('#rqb .gwt-ListBox option:first-child').attr('disabled','disabled');
         }
-      })
+      });
 
-  }
+      /* DOWNLOAD AS CSV BUTTON BEHAVIOR */
+      $download_csv_btn.click(function(){
+        var q = $sql_query_textarea.val();
+        var ajax_img = '<img src="web/images/ajax-loader.gif"/>';
+        $(this).html('Fetching... ' + ajax_img);
+        convertJSONtoCSV(q);
+      });
+
+  };
   function scrollThere(that, e){
     e.preventDefault();
     target = that.hash;
@@ -67,17 +85,29 @@ $(function() {
           }else{
             document.getElementById("sql").value = sanitize_out(out);
           }
-          document.getElementById("download-json").setAttribute('href', query('https://premium.scraperwiki.com/cc7znvq/47d80ae900e04f2/sql/?q=', out));
-          document.getElementById("download-csv").setAttribute('href', query('http://jsonadapter.herokuapp.com/?q=', out));
+          document.getElementById("download-json").setAttribute('href', query(api_endpoint, out));
         },
       });
+  };
+
+  function convertJSONtoCSV(query){
+    fetchJSON(query).done(function(json){
+      $download_csv_btn.html('Download .csv')
+      var csv = dsv.csv.format(json);
+      window.location.href = "data:text/csv," + encodeURIComponent(csv);
+    });
+  };
+
+  function fetchJSON(query){
+    return $.ajax({
+      url: api_endpoint + query
+    });
   };
 
   $.get('web/table_schema/tables.json', function(table_schema) {
     initRedQuery(table_schema);
     bindHandlers();
   });
-
 
 
 });
